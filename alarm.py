b7a82c246d16574858b4e232d035f9e6d826c0a7
David <godswilludohdavid@gmail.com>
2025-10-17 20:26:14 +0100

    Ive fixed the issue with the sound not playing. The issue occured because the program was shutting down before the music had a chance.
    I tried different ways to solve this, such as a loop - which just made
    the music play on top of one another
    
    I also tried using timers - but i thought there had to be a better way.
    
    So i landed on using an input function - which to be honest i dont know
    why i didnt think of that earlier.
    
    The program works fine now.
    
    I'm also going to add the music file to this dir for easier accesss.
    
    Thank you.

--- alarm.py
+++ alarm.py
@@ -1,39 +1,43 @@
-import os
 import argparse
 import time
 from pygame import mixer
-
-mixer.init()
 
 parser = argparse.ArgumentParser(description="Type in the amount of time")
 parser.add_argument("time", help="Type in the amount of time")
 args = parser.parse_args()
 
-timer = args.time
+def convert():
+    timer = args.time
+    hours, mins, sec = list(map(int, timer.split(":")))
+    hours = hours * 60
+    mins = mins + hours
+    mins = mins * 60
+    sec = sec + mins
+    return sec
 
-hours, mins, sec = list(map(int, timer.split(":")))
-
-hours = hours * 60
-mins = mins + hours
-mins = mins * 60
-sec = sec + mins
+sec = convert()
 
 def countdown(sec):
-    for n in range(sec):
-        print(time.strftime("%H:%M:%S",time.gmtime(sec)), end="\r")
+    for i in range(sec):
+        print(time.strftime("%H:%M:%S", time.gmtime(sec)), end="\r")
         sec -= 1
         time.sleep(1)
 
-def playsong():
-    print("Done here")
+def playsound():
+    mixer.init()
     mixer.music.load("/home/david/Music/Washing.mp3")
-    mixer.music.play(0)
+    mixer.music.play(-1)
+
+def stop():
+    input("Time's up buddy...")
+
+def timer():
+    print(f"Alarm set for {time.strftime("%H:%M:%S",time.gmtime(sec))}")
+    countdown(sec)
+    playsound()
+    stop()
 
 try:
-    while True:
-        print(f"Alarm set for {time.strftime("%H:%M:%S",time.gmtime(sec))}")
-        countdown(sec)
-        playsong()
-        break
+    timer()
 except KeyboardInterrupt:
-    print("Uhh Oh")
+    print("Bye buddy")

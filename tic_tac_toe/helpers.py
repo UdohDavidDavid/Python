6db194c3b22c3460539af8fc88ac31a4e0e27e53
David <godswilludohdavid@gmail.com>
2025-10-20 20:57:31 +0100

    Added comments

--- tic_tac_toe/helpers.py
+++ tic_tac_toe/helpers.py
@@ -1,4 +1,4 @@
-import sys
+import sys # To quit the app
 
 # This prints the board to the console
 def draw_board(spots):
@@ -11,10 +11,12 @@ def check_turn(turn):
     else:   return "X"
 
 def stop_game(turn):
-    player = check_turn(turn)
+    player = check_turn(turn)  # Gets who the current player is
     print(f"{player} has won!")
-    sys.exit()
+    sys.exit() # Quits the game
 
+# Checks available win slots
+# Returns true if a player has won and returns false if a player has not won
 def check_winner(spots, turn):
     if spots[1] == spots[2] == spots[3] or spots[4] == spots[5] == spots[6] or spots[7] == spots[8] == spots[9]:
         stop_game(turn)
@@ -30,7 +32,8 @@ def check_winner(spots, turn):
 
 
 def check_tie(spots, turn):
-    winning = check_winner(spots, turn)
+    winning = check_winner(spots, turn) # Checks if player has won or not
+    # Checks for a tie
     if turn >= 9 and winning == False:
         print("tie")
         sys.exit()
